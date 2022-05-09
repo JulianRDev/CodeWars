7637a5438ba382750dacea6b94a116e30e2fc9b3
Julian Rosa <rosajulian1930@gmail.com>
2022-05-08 20:11:23 -0400

    GrassHopper

---
+++ 8kyu/grasshopper.js
@@ -0,0 +1,17 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+
+
+
+
+
+function greet (name, owner) {
+    // Add code here
+    if (name === owner){
+      return 'Hello boss'
+    }else{
+      return 'Hello guest'
+    }
+  }
