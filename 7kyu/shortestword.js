639c1899f5a3a45307833ad7545006f0525f50d4
Julian Rosa <rosajulian1930@gmail.com>
2022-07-01 23:03:43 -0400

    Shortest Word

---
+++ 7kyu/shortestword.js
@@ -0,0 +1,15 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+function findShort(s){
+  
+    let word = s.split(' ')
+  
+    let shortWord = word.sort((a,b) => {
+      return a.length - b.length
+    })
+    
+    return shortWord[0].length
+  }
